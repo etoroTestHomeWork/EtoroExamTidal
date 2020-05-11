2820441ca296b52a0ef1f3143c4611d345c5608b
Andrey <dodon.andrey@gmail.com>
2020-05-11 12:04:51 +0300

    Revert "Revert "Accidentally removed <package id = "MSTest.TestAdapter" version = "1.3.2" targetFramework = "net48" />""
    This reverts commit 22e7cb2d87afb7e9a234894ad1f3fbd475c6f7d8.

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62841e7d-19b9-46df-9f0c-75b9bdbc3a71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
